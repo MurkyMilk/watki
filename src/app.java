4720bb1800a04b48b70be76722e62fdc0885598c
Mleko <you@example.com>
2016-11-08 22:35:57 +0100

    Initial commit

---
+++ src/app.java
@@ -0,0 +1,61 @@
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+
+class watki {
+
+    public static final int FIBONACCI_NUM_TO_CALCULATE = 45;
+    public static final int IERATIONS_NUM = 8;
+
+    public static long fibonacci(int n) {
+        if (n <= 1) return n;
+        else return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        long startTime = System.currentTimeMillis();
+        multi_test(FIBONACCI_NUM_TO_CALCULATE,  IERATIONS_NUM);
+        System.out.println("TIME: " + (System.currentTimeMillis() - startTime));
+
+        startTime = System.currentTimeMillis();
+        single_test(FIBONACCI_NUM_TO_CALCULATE, IERATIONS_NUM);
+        System.out.println("TIME: " + (System.currentTimeMillis() - startTime));
+
+    }
+
+    private static void single_test(int fibonacci_num_to_calculate, int num_of_iterations) {
+        IntStream.range(0, num_of_iterations).forEachOrdered(n ->
+            System.out.println(fibonacci(fibonacci_num_to_calculate))
+        );
+    }
+
+    private static void multi_test(int fibonacci_num_to_calculate, int num_of_iterations) throws InterruptedException {
+        ExecutorService executor = Executors.newWorkStealingPool();
+        List<Callable<Long>> methods_to_call = new LinkedList<>();
+
+        IntStream.range(0, num_of_iterations).forEachOrdered(n ->
+        methods_to_call.add(()->fibonacci(fibonacci_num_to_calculate))
+        );
+
+        executor.invokeAll(methods_to_call)
+                .stream()
+                .map(future -> {
+                    try {
+                        return future.get();
+                    }
+                    catch (Exception e) {
+                        throw new IllegalStateException(e);
+                    }
+                })
+                .forEach(System.out::println);
+    }
+
+}
+
+
+
